9ac106d90f53926ddcc6586bc71138e9b04be602
Andrew Kita <andrewk0792@yahoo.com>
2022-01-11 11:20:46 -0800

    Created and connected new modules for home page hero animations.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import loadHamburgerMenu from './modules/navigation/hamburger';
+import loadHeroAnimations from './modules/animations/hero'; 
+// index is implied directory for the above
 
 loadHamburgerMenu();
-
+loadHeroAnimations();
---
+++ src/modules/animations/hero/index.js
@@ -0,0 +1,9 @@
+console.log('hello world from loadHeroAnimations!');
+function loadHeroAnimations() {
+    const animateColumnLeftElement = document.getElementById('animateColumnLeft');
+
+    animateColumnLeftElement.classList.remove('opacity');
+};
+ 
+export default loadHeroAnimations;
+
